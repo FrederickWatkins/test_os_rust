82149190f223437a94ea50cc87016b136a91554c
Frederick Watkins <freddiewatkins6@gmail.com>
2022-11-05 22:55:35 +0000

    Added print and println macros

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,9 @@ mod vga_buffer;
 // The entry point for the OS
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    use core::fmt::Write;
-    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
-    write!(vga_buffer::WRITER.lock(), " Some numbers: {} {} \n", 42, 1.337).unwrap();
-    write!(vga_buffer::WRITER.lock(), "I'm on the next line now! \n").unwrap();
-    write!(vga_buffer::WRITER.lock(), "This line is so looooooooooooooooooooooooooooooooooooooong it wraps onto the next one! \n").unwrap();
+    println!("Some numbers: {} {}", 42, 1.337);
+    println!("I'm on the next line now! ");
+    println!("This line is so looooooooooooooooooooooooooooooooooooooong it wraps onto the next one! ");
 
     loop {}
 }
