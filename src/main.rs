8646a9afb04a5e71e0d197e451329434a751cbbc
Frederick Watkins <freddiewatkins6@gmail.com>
2022-11-05 20:01:19 +0000

    Created static writer

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,12 @@ mod vga_buffer;
 // The entry point for the OS
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    use core::fmt::Write;
+    vga_buffer::WRITER.lock().write_str("Hello World!").unwrap();
+    write!(vga_buffer::WRITER.lock(), " Some numbers: {} {} \n", 42, 1.337).unwrap();
+    write!(vga_buffer::WRITER.lock(), "I'm on the next line now! \n").unwrap();
+    write!(vga_buffer::WRITER.lock(), "This line is so looooooooooooooooooooooooooooooooooooooong it wraps onto the next one! \n").unwrap();
+
     loop {}
 }
 
