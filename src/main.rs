4f9081bc5228e610fdeb3844b76683254b0adc88
Frederick Watkins <freddiewatkins6@gmail.com>
2022-11-06 14:59:13 +0000

    Moved vga_buffer to seperate library

--- src/main.rs
+++ src/main.rs
@@ -4,10 +4,11 @@
 #![test_runner(qemu_test::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-mod vga_buffer;
-
 #[cfg(not(test))]
 use core::panic::PanicInfo;
+use vga_buffer::println;
+use vga_buffer::Color;
+use vga_buffer::WRITER;
 
 // The entry point for the OS
 #[no_mangle]
@@ -16,13 +17,14 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("Yabba dabba doo!");
+    WRITER.lock().set_color(Color::Black, Color::White);
+    println!("This should be a different colour now");
     panic!("Test panic");
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    
     println!("{}", info);
     loop {}
 }
---
+++ tests/basic_boot.rs
@@ -0,0 +1,15 @@
+#![no_std]
+#![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(qemu_test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
+
+// The entry point for the OS
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    test_main();
+
+    loop{}
+}
+
